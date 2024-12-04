848b5ab3bffdb3c38fca5087092f0a01d8181f28
Harsh <xdenvil0@gmail.com>
2024-12-05 02:02:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,20 @@
 
 
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 import sentencepiece
-import os
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Telegram Bot Token
-TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+TOKEN = "5762561230:AAHYeayO4kdUIPIMvJZrzv-x-qiJjpZpIgo"
 
 # Model and Tokenizer
 model_name = 't5-small'
@@ -32,29 +31,21 @@ nltk.download('punkt')
 # Sentiment Analysis
 sia = SentimentIntensityAnalyzer()
 
-def generate_response(input_text: str) -> str:
-    try:
-        inputs = tokenizer(input_text, return_tensors="pt")
-        outputs = model.generate(inputs["input_ids"], max_length=100)
-        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return response
-    except Exception as e:
-        logger.error(f"Error generating response: {e}")
-        return "Error generating response"
+def generate_response(input_text):
+    inputs = tokenizer(input_text, return_tensors="pt")
+    outputs = model.generate(inputs["input_ids"], max_length=100)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
 
-def sentiment_analysis(input_text: str) -> str:
-    try:
-        sentiment_scores = sia.polarity_scores(input_text)
-        compound_score = sentiment_scores['compound']
-        if compound_score >= 0.05:
-            return "Positive"
-        elif compound_score <= -0.05:
-            return "Negative"
-        else:
-            return "Neutral"
-    except Exception as e:
-        logger.error(f"Error analyzing sentiment: {e}")
-        return "Error analyzing sentiment"
+def sentiment_analysis(input_text):
+    sentiment_scores = sia.polarity_scores(input_text)
+    compound_score = sentiment_scores['compound']
+    if compound_score >= 0.05:
+        return "Positive"
+    elif compound_score <= -0.05:
+        return "Negative"
+    else:
+        return "Neutral"
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm SHAN AI.")
@@ -70,12 +61,10 @@ def echo(update, context):
 
 def main():
     updater = Updater(TOKEN)
-
     dispatcher = updater.dispatcher
 
     start_handler = CommandHandler('start', start)
     help_handler = CommandHandler('help', help_command)
-
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 
     dispatcher.add_handler(start_handler)
