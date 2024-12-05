aa85ffd62b5b2b33f8ca9fb772267dc34bf0473a
Harsh <xdenvil0@gmail.com>
2024-12-05 17:52:11 +0530

    Create profile_access.py

---
+++ `profile_access/profile_access.py
@@ -0,0 +1,18 @@
+
+```
+import json
+
+def access_profile(profile_data, entities, intent):
+    if intent == 'ask_about_profile':
+        if 'name' in entities:
+            return profile_data['name']
+        elif 'description' in entities:
+            return profile_data['description']
+        elif 'creator' in entities:
+            return profile_data['creator']
+    elif intent == 'ask_about_ai':
+        if 'abilities' in entities:
+            return profile_data['abilities']
+        elif 'knowledge' in entities:
+            return profile_data['knowledge']
+```
